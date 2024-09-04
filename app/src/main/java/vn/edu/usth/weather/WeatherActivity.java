fd9dfce092dea2acdc9f00d40ab3d67ed92313c0
Peter Packerson <vunhat20032008@gmail.com>
2024-09-04 23:18:07 +0700

    Practical Work 3

--- app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -1,6 +1,7 @@
 package vn.edu.usth.weather;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +21,40 @@ public class WeatherActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        Log.i("start-point", "App Start");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        Log.i("resume-point", "App Resume");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        Log.i("pause-point", "App Pause");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        Log.i("stop-point", "App Stop");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        Log.i("destroy-point", "App Destroy");
     }
 }
